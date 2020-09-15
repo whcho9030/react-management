350653071273a6589555d6ca23d8b58afa89013f
whcho9030 <whcho9030@gmail.com>
2020-09-15 18:58:05 +0900

    Create React Project!!

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends React.Component {
   render() {
     return (
       <div className="gray-background">
-        <img src={logo} lat="logo" />
+        <img src={logo} lat="logo"  alt="logo"/>
         <h2>Let's develop management system!</h2>
       </div>
     );
